b28a9c15bc6ae91571cd31244a97d81bf3526cf3
Sibi Sabesan <64496976+sibisabesan26@users.noreply.github.com>
2025-11-14 23:14:34 -0500

    Delete models directory

--- models/Task.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mobile_project.models
-
-data class Task(
-    val id: Int,
-    val title: String,
-    val description: String,
-    var isCompleted: Boolean
-)
-
--- models/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.mobile_project.models
-
-data class User(val name: String, val email: String, val password: String)
